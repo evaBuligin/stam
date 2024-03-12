0fc97bf26c32646ae9a6efac02f82ed119b6a0b4
eva <bulevelyn01@gmail.com>
2024-03-12 19:09:46 +0200

    alina's embarrassing moments in life

--- f1.py
+++ f1.py
@@ -1,8 +1,7 @@
 def multiply():
-    a = float(input("number 1: "))
-    b = float(input("number 1: "))
 
-    print(f"result: {a * b}")
+    print(f" one day, alina and tohar and i were talking about life. suddenly, alina started to gossip about one girl from our class, only to find out this gorl stand right behind her. alina was very very embarressed.")
+    print(f"bonus: alina says hok a shavot instead of hok a shvut.")
 
 
 multiply()
